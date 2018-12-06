af0337935dc8a960d6be8b2690c93573aa7a472f
Domingo Brandt <brandtyet02@gmail.com>
2018-12-05 20:51:42 -0400

    google analitycs verification

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/googlecb77e256ced0f5e3.html', function () {
+    return view('googlecb77e256ced0f5e3');
+});
+
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
