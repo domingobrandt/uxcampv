0aa45c2b952171d5acc76d48ba2de0aa15532658
Domingo Brandt <brandtyet02@gmail.com>
2018-12-06 13:26:13 -0400

    menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 
-                {!!menu('main','bootstrap','mymenu')!!}
+                {!!menu('main','bootstrap')!!}
 
     <div class="container">
             <h1>{{setting('admin.title')}} </h1> 
